66e119aec0a5f90a2962320ecb9c15f0e3180b29
liana <garaeva.lianaa@gmail.com>
2024-03-18 21:27:55 +0500

    webinar-19

---
+++ tests/newTest.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Auth test cases', async () => {
+  //   test('Check login', async ({ page }) => {
+  //     await page.goto('https://demoqa.com/login')
+  //     await page.locator('input[id=userName]').fill('Myrtice21')
+  //     await page.locator('input[id=password]').fill('test12345')
+  //     // await page.locator('button[id=login]').click()
+  //     // await expect(page.locator('#userForm')).toBeVisible()
+  //     await expect(page).toHaveScreenshot('loginPage.png')
+})
+test('Check profile', async ({ page }) => {
+  await page.goto('https://demoqa.com/profile')
+  await expect(page.locator('#userName-value')).toBeVisible()
+})
